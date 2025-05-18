75c1b061d90b5cb1bd43a357b3688efbe66304f7
Cristian Chavez <nato6293@gmail.com>
2025-05-17 20:33:21 -0500

    metodos de registar, iniciar sesion y cerrar sesion

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
+  output:"server",
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [db()]
 });
